aa3d586337c7ba722862025cdbffbd96422651da
Eric Thompson <eric.r.thompson@icloud.com>
2016-04-16 23:39:52 -0400

    Improved planet generator

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -10,6 +10,7 @@ public class GameMaster : MonoBehaviour
     public Transform explosionEffect;
     public float respawnTime = 3;
     public float numPlanets = 8;
+    public bool randomPlanets = false;
 
     // Use this for initialization
     void Start () {
@@ -20,13 +21,27 @@ public class GameMaster : MonoBehaviour
     }
     
     public void SpawnPlanets() {
-        int type;        
+        int type = planetPrefabs.Length;
+        int planetSpacing = 5000;
         Vector3 pos;
         
         for (int i = 0; i < numPlanets; i++) {
-            type = Random.Range(0, planetPrefabs.Length);
-            pos = new Vector3 (Random.Range(-20000, 20000), Random.Range(-20000, 20000), 0);
+            if (randomPlanets) {
+                type = Random.Range(0, planetPrefabs.Length);                
+            } else {
+                type++;
+                if (type > planetPrefabs.Length) {
+                    type = 0;
+                }
+            }
+            Debug.Log(i);
+            do {
+                pos = new Vector3 (Random.Range(-planetSpacing * (i+1), planetSpacing * (i+1)),
+                    Random.Range(-planetSpacing * (i+1), planetSpacing * (i+1)), 0);
+            } while (pos.magnitude < planetSpacing * (i+1));
+            
             Instantiate(planetPrefabs[type], pos, Quaternion.identity);
+            
         }
     }
     
