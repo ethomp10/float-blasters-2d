affb9175718f13ee0f6f4d203507b18a0b376e43
Oskold <Fletcher_gray@hotmail.com>
2016-04-20 20:17:58 -0400

    Fixed compass

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -11,8 +11,12 @@ public class GameMaster : MonoBehaviour
     public float numPlanets = 8;
     public bool randomPlanetTypes = false;
 
+    private string[] planetNames = { "Kornax", "Home", "Nebrol", "Hath" };
+
     private Transform spawnPoint;
+    private Transform tempPlanet;
     private Transform homePlanet;
+    private Planet pScript;
 
     // Use this for initialization
     void Start () {
@@ -27,26 +31,41 @@ public class GameMaster : MonoBehaviour
         int type = planetPrefabs.Length;
         int planetSpacing = 5000;
         Vector3 pos;
-        
-        for (int i = 0; i < numPlanets; i++) {
-            if (randomPlanetTypes) {
-                type = Random.Range(0, planetPrefabs.Length);                
-            } else {
+
+        for (int i = 0; i < numPlanets; i++)
+        {
+            if (randomPlanetTypes)
+            {
+                type = Random.Range(0, planetPrefabs.Length);
+            }
+            else
+            {
                 type++;
-                if (type >= planetPrefabs.Length) {
+                if (type >= planetPrefabs.Length)
+                {
                     type = 0;
                 }
             }
-            do {
-                pos = new Vector3 (Random.Range(-planetSpacing * (i+1), planetSpacing * (i+1)),
-                    Random.Range(-planetSpacing * (i+1), planetSpacing * (i+1)), 0);
-            } while (pos.magnitude < planetSpacing * (i+1));
-            
-            if (i == 1)
-                homePlanet = Instantiate(planetPrefabs[type], pos, Quaternion.identity) as Transform; 
-            else
-                Instantiate(planetPrefabs[type], pos, Quaternion.Euler(0, 0, Random.Range(0, 359)));
+            do
+            {
+                pos = new Vector3(Random.Range(-planetSpacing * (i + 1), planetSpacing * (i + 1)),
+                    Random.Range(-planetSpacing * (i + 1), planetSpacing * (i + 1)), 0);
+            } while (pos.magnitude < planetSpacing * (i + 1));
+
+            if (i == 1) {
+                homePlanet = Instantiate(planetPrefabs[type], pos, Quaternion.identity) as Transform;
+                //GameMaster.KillPlayer(spaceship.gameObject);
+                pScript = homePlanet.GetComponent<Planet>();
+                pScript.NamePlanet(planetNames[i]);
+                pScript.GetCompass(i);
+            } else {
+                tempPlanet = Instantiate(planetPrefabs[type], pos, Quaternion.Euler(0, 0, Random.Range(0, 359))) as Transform;
+                pScript = tempPlanet.GetComponent<Planet>();
+                pScript.NamePlanet(planetNames[i]);
+                pScript.GetCompass(i);
+            }
         }
+
         spawnPoint = homePlanet.GetChild(3).transform;
     }
 
@@ -57,7 +76,6 @@ public class GameMaster : MonoBehaviour
 	public IEnumerator RespawnPlayer () {
 		yield return new WaitForSeconds (respawnTime);
         Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
-
     }
 
     public static void KillPlayer (GameObject player) {
